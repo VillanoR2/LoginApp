48d9bdc31d2490bc0d03a507044b17754c5457e0
VillanoR2 <villa.artu15@gmail.com>
2022-05-23 17:21:15 -0500

    Fix POST not supported

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -35,7 +35,7 @@ function sendAuthCodeToServer(authCode) {
     body: formBody //
   };
 
-  fetch("signin.bindid-sandbox.io/token", requestOptions)
+  fetch("https://signin.bindid-sandbox.io/token", requestOptions)
   .then((response) => {
     if(response.ok){
       response.json((json) => {
