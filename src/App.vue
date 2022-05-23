553ce488a0fa97eb48689224dd0d2d49f1e8d6f4
VillanoR2 <villa.artu15@gmail.com>
2022-05-23 16:39:31 -0500

    test 2...

--- src/App.vue
+++ src/App.vue
@@ -29,43 +29,43 @@ export default {
             nombre: ""
         }
     },
-    mounted() {
-        let metaOne = document.createElement("meta");
-        metaOne.setAttribute("name", "xm-bind-id-client_id");
-        metaOne.setAttribute("content", "fb355573.417b53dd.tid_414530e0.bindid.io");
-        metaOne.setAttribute("id", "metaTransmitOne");
-        document.head.appendChild(metaOne);
+    // mounted() {
+    //     let metaOne = document.createElement("meta");
+    //     metaOne.setAttribute("name", "xm-bind-id-client_id");
+    //     metaOne.setAttribute("content", "fb355573.417b53dd.tid_414530e0.bindid.io");
+    //     metaOne.setAttribute("id", "metaTransmitOne");
+    //     document.head.appendChild(metaOne);
 
-        let metaTwo = document.createElement("meta");
-        metaTwo.setAttribute("name", "xm-bind-id-redirect_uri");
-        metaTwo.setAttribute("content", "https://loginapp-three.vercel.app/user");
-        metaTwo.setAttribute("id", "metaTransmitTwo");
-        document.head.appendChild(metaTwo);
+    //     let metaTwo = document.createElement("meta");
+    //     metaTwo.setAttribute("name", "xm-bind-id-redirect_uri");
+    //     metaTwo.setAttribute("content", "https://loginapp-three.vercel.app/user");
+    //     metaTwo.setAttribute("id", "metaTransmitTwo");
+    //     document.head.appendChild(metaTwo);
 
-        let scriptOne = document.createElement("script");
-        scriptOne.setAttribute("src", "https://polyfill.io/v3/polyfill.min.js?features=Promise%2CPromise.prototype.finally%2CTextDecoder%2CTextEncoder%2CObject.entries");
-        scriptOne.setAttribute("id", "scrpitTransmitOne");
-        document.head.appendChild(scriptOne);
+    //     let scriptOne = document.createElement("script");
+    //     scriptOne.setAttribute("src", "https://polyfill.io/v3/polyfill.min.js?features=Promise%2CPromise.prototype.finally%2CTextDecoder%2CTextEncoder%2CObject.entries");
+    //     scriptOne.setAttribute("id", "scrpitTransmitOne");
+    //     document.head.appendChild(scriptOne);
 
-        let scriptTwo = document.createElement("script");
-        scriptTwo.setAttribute("src", "https://signin.bindid-sandbox.io/bindid-sdk/transmit-bind-id-sdk.js");
-        scriptTwo.setAttribute("id", "scrpitTransmitTwo");
-        scriptTwo.defer = true
-        document.head.appendChild(scriptTwo);
-    },
-    unmounted() {
-        let scriptOne = document.getElementById("scrpitTransmitOne")
-        document.head.removeChild(scriptOne);
+    //     let scriptTwo = document.createElement("script");
+    //     scriptTwo.setAttribute("src", "https://signin.bindid-sandbox.io/bindid-sdk/transmit-bind-id-sdk.js");
+    //     scriptTwo.setAttribute("id", "scrpitTransmitTwo");
+    //     scriptTwo.defer = true
+    //     document.head.appendChild(scriptTwo);
+    // },
+    // unmounted() {
+    //     let scriptOne = document.getElementById("scrpitTransmitOne")
+    //     document.head.removeChild(scriptOne);
 
-        let scriptTwo = document.getElementById("scrpitTransmitTwo")
-        document.head.removeChild(scriptTwo);
+    //     let scriptTwo = document.getElementById("scrpitTransmitTwo")
+    //     document.head.removeChild(scriptTwo);
 
-        let metaOne = document.getElementById("metaTransmitOne")
-        document.head.removeChild(metaOne);
+    //     let metaOne = document.getElementById("metaTransmitOne")
+    //     document.head.removeChild(metaOne);
 
-        let metaTwo = document.getElementById("metaTransmitTwo")
-        document.head.removeChild(metaTwo);
-    },
+    //     let metaTwo = document.getElementById("metaTransmitTwo")
+    //     document.head.removeChild(metaTwo);
+    // },
     methods: {
     }
 }
